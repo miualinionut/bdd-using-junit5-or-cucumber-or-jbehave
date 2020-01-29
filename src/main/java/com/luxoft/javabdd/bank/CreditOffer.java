e271210d50880dd0614af8cb6ca54c05926c3273
training <training@ro-ptc-28.ptc.local>
2020-01-29 10:42:40 +0200

    100% code coverage with cucumber

--- src/main/java/com/luxoft/javabdd/bank/CreditOffer.java
+++ src/main/java/com/luxoft/javabdd/bank/CreditOffer.java
@@ -6,33 +6,35 @@ import java.util.List;
 
 public abstract class CreditOffer {
 
-	private String id;
-	private List<Customer> customersList = new ArrayList<Customer>();
-	private String creditOfferType;
+    private String id;
+    private List<Customer> customersList = new ArrayList<Customer>();
+    private String creditOfferType;
 
-	public CreditOffer(String id, String creditOfferType) {
-		this.id = id;
-		this.creditOfferType = creditOfferType;
-	}
+    public CreditOffer(String id, String creditOfferType) {
+        this.id = id;
+        this.creditOfferType = creditOfferType;
+    }
 
-	public List<Customer> getCustomersList() {
-		return Collections.unmodifiableList(customersList);
-	}
+    public List<Customer> getCustomersList() {
+        return Collections.unmodifiableList(customersList);
+    }
 
-	protected boolean addToCustomersList(Customer customer) {
-		if(this.customersList.contains(customer)) {
-			return false;
-		} else {
-			return this.customersList.add(customer);
-		}
-	}
+    protected boolean addToCustomersList(Customer customer) {
+        if (this.customersList.contains(customer)) {
+            return false;
+        } else {
+            System.out.println(customer.getName() + " added");
+            return this.customersList.add(customer);
+        }
+    }
 
-	protected boolean removeFromCustomersList(Customer customer) {
-		return this.customersList.remove(customer);
-	}
+    protected boolean removeFromCustomersList(Customer customer) {
+		System.out.println(customer.getName() + " removed");
+        return this.customersList.remove(customer);
+    }
 
-	public abstract boolean addCustomer(Customer customer);
+    public abstract boolean addCustomer(Customer customer);
 
-	public abstract boolean removeCustomer(Customer customer);
+    public abstract boolean removeCustomer(Customer customer);
 
 }
